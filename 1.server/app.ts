ef080e0bc9c16ad11fd056c5e67319e6660eccb8
Mika Turunen <mika.turunen@ymail.com>
2014-11-08 11:55:36 +0200

    Dropped few minor TODO notes

--- 1.server/app.ts
+++ 1.server/app.ts
@@ -1,20 +1,21 @@
 var express = require("express");
-var app 	= express();
-var path    = require("path");
+var app = express();
+var path = require("path");
 
 var staticPublichPath = path.normalize(path.join(__dirname, "..", "client"));
 
 app.use("/public", express.static(staticPublichPath));
 
+// TODO we need to start specifying more details routes 
 app.get("*", (req: any, res: any) => {
   var indexHtml: string = path.join(__dirname, "..", "client", "html", "index.html");
-  console.log("path for index: " + indexHtml);
 	res.sendFile(indexHtml);
 });
 
+// TODO read details from configuration
 var server = app.listen(3000, "127.0.0.1", () => {
 	var host = server.address().address;
 	var port = server.address().port;
 
-	console.log("Example app listening at http://%s:%s", host, port);
+	console.log("GameShelves running at http://%s:%s", host, port);
 });
