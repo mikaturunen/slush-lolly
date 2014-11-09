29731251061ccd79345c9434a8483feabf4b6bfe
Mika Turunen <mika.turunen@ymail.com>
2014-11-09 18:27:41 +0200

    New definitions for server side: mongoose from DefinitelyTyped

--- definitions/server.d.ts
+++ definitions/server.d.ts
@@ -3,5 +3,8 @@
  * Collecting all of them into a single file as I'm lazy like that...
  */
 
+///<reference path="DefinitelyTyped/mongoose/mongoose.d.ts" />
+
 ///<reference path="DefinitelyTyped/node/node.d.ts" />
 ///<reference path="DefinitelyTyped/express/express.d.ts" />
+
