9fb77773c3d57d2e309f646bf6ace0d476092d5a
Mika Turunen <mika.turunen@ymail.com>
2014-11-09 18:11:34 +0200

    Starting to put together first pieces of the collection module for backend

---
+++ 1.server/collection/collection.ts
@@ -0,0 +1,11 @@
+import mongo = require("mongoose");
+
+/**
+ * @module collection.
+ * Used to communicate Collection documents back and forth to the mongodb.
+ */
+module collection {
+
+}
+
+export = collection;
