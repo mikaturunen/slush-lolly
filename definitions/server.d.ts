8ec94526320511e6bb5cd313e20c15a67e5497f2
Mika Turunen <mika.turunen@ymail.com>
2014-11-23 19:17:00 +0200

    Using the socket.io server side definitions on the server.d.ts

--- definitions/server.d.ts
+++ definitions/server.d.ts
@@ -7,3 +7,4 @@
 
 ///<reference path="DefinitelyTyped/node/node.d.ts" />
 ///<reference path="DefinitelyTyped/express/express.d.ts" />
+///<reference path="DefinitelyTyped/socket.io/socket.io.d.ts" />
